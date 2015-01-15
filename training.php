1d543a6867ebd892633c8d1605a4844310895d11
hmajid2301 <hmajid2301@gmail.com>
2015-01-15 23:17:09 +0000

    finished team page

---
+++ training.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--Add header data (such as import stylesheets) -->
+    <?php include 'php/head.php' ?>
+    <title>Training</title>
+  </head>
+  <body>
+    <div id="wrapper">
+      <!--Add header including logo, and navbar elements -->
+      <?php include 'php/header.php' ?>
+      <div class="container">
+        <div class="row">
+          <div class="training">
+            <h2 class="section">Training Information</h2>
+
+            <h3>When and Where?</h3><br>
+            <p class="larger-font">  
+              Tuesday: 20:30 - 22:00 <br>
+              Thursday: 19:00 - 20:30 <br>
+              Saturday: 14:00 - 16:00 <br>
+            </p>
+
+            <p class="larger-font">
+            All sessions are at <a href="images/map.jpg">Westwood Games Hall</a>.Try to turn to lessons 10-15 minutes early. Please wear a clean dobok (uniform) to each formal training session. Beginners, please bring some loose-fitting clothing such as a t-shirt and shorts/trousers to provide freedom of movement. As a safety requirement, training is done barefoot and we request that you remove any jewellery. <br>
+            </p>
+
+            <h3>Cost</h3><br>
+            <p class="larger-font">
+            The first <b>THREE</b> lessons are free. Other than that the lessons cost <br><br>
+            Per Session: £3 <br>
+            Per Term: £30 <br>
+            Per Year: £60 <br> <br>
+
+            To officially join the club (and be covered by the Warwick Sport insurance), login to our   <a href="http://warwicksport.warwick.ac.uk/clubs/taekwondo/" target="_blank">Warwick Sports page </a>and follow the instructions. <br><br>
+
+            If you wish to grade to a higher belt you will eventually need to buy a dobok (the white suit) which can be purchased through the club. If you want to pursue sparring then protective gear is available for purchase in Term 2 although this is not essential to continue training.
+            </p>
+            <br><br><br>
+          </div> <!-- End of training -->
+        </div> <!-- End of row -->
+      </div> <!-- End of container -->
+    </div><!--End of wrapper -->
+    <!--Add footer, contains 3 columns layout Add footer data, such as jquery, boostrap javascript -->
+    <?php include 'php/footer.php' ?>
+  </body>
+</html>
+
