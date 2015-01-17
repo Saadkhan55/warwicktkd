094e05173d4946089570f4261d0085ca1b4a24ee
hmajid2301 <hmajid2301@gmail.com>
2015-01-17 13:54:02 +0000

    contact form finished

--- contact.php
+++ contact.php
@@ -1,3 +1,85 @@
+<?php
+//This php script will validate form fields
+//and returns errors if the fields aren't valid
+//else it will send the email out to to warwick
+//tkd
+
+
+  $sent = false;
+  $error = false;
+
+  //if sent button was clicked with method sent to post
+  if(isset($_POST['submit'])) {
+    //trim the fields and remove html character to escape them
+    $name = trim(htmlspecialchars($_POST['name'], ENT_QUOTES));
+    $email = trim($_POST['email']);
+    $message = trim(htmlspecialchars($_POST['message'], ENT_QUOTES));
+
+    $fieldsArray = array (
+      'name' => $name,
+      'email' => $email,
+      'message' => $message
+    );
+
+    //will store all errors in this array
+    $errorArray = array();
+
+    //for each element in form (i.e. in fields array) validate them
+    foreach($fieldsArray as $key => $val) {
+      switch($key) {
+        //if name field then check for emptiness
+        case 'name':
+          if(empty($val)) {
+            $error = true;
+            $errorArray[$key] = ucfirst($key) . " field was left empty";
+          }
+         break;    
+
+        //do the same for message field
+        case 'message':
+          if(empty($val)) {
+            $error = true;
+            $errorArray[$key] = ucfirst($key) . " field was left empty";
+          }    
+          break;
+
+        case 'email':
+          //check if email is invalid add error
+          if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $error = true;
+            $errorArray[$key] = "Invalid Email Address entered";
+          }
+          //else if valid sanitize email
+          else {
+             $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+          }
+          break;
+      }
+    }
+
+    // if no errors then sent email to webmaster
+    if(!$error) {
+      $to = "me@haseebmajid.com";
+      $subject = "contact form ";
+      $msgcontent = "Namei: $name<br> Email: $email <br>Message: $message";
+      $headers = "MIME_Version: 1.0 \r\n";
+      $headers .= "Content-type: text/html; charset=iso-8859-1 \r\n";
+      $headers .= "From: $name <$email> \r\n";
+      $mailsent = mail($to, $subject, $msgcontent, $headers);
+
+      //if mail sent is true 
+      if($mailsent) {
+        //set sent variable to true
+        $sent = true;
+        //unset all variables;
+        unset($name);
+        unset($email);
+        unset($message);
+      }
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -14,34 +96,50 @@
 
           <div class="col-md-12">
             <div class="well well-sm">
-              <form class="form-horizontal" method="post" action="mailto.php">
+
+            <form class="form-horizontal" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate>
                 <fieldset>
                   <h1 class="text-center header">Contact Us</h1>
+                  
+                  <?php
+                    //if message was sent successfully without any errors then print out a success message
+                    if($sent) {
+                      echo "<h2 class='success'>Thank you for the message the form was sent successfully</h2>";
+                    }
+                    //else list all errors
+                    else if ($error) {
+                      echo '<ul class="error-list unlisted">';
+                      foreach($errorArray as $key => $val) {
+                        echo "<li>" . ucfirst($key) . ": field error - $val </li>";
+                      }
+                      echo '</ul>';
+                    }
+                  ?>
 
                   <div class="form-group">
                     <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                     <div class="col-md-8">
-                      <input id="fname" name="name" type="text" placeholder="Full Name" class="form-control">
+                    <input id="fname" name="name" type="text" placeholder="Full Name" value="<?php echo (isset($name) ? $name : ""); ?>"class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
                     <div class="col-md-8">
-                      <input id="email" name="email" type="text" placeholder="Email Address" class="form-control" required>
+                      <input id="email" name="email" type="text" placeholder="Email Address" value="<?php echo (isset($email) ? $email : ""); ?>" class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
                     <div class="col-md-8">
-                      <textarea class="form-control" id="message" name="message" placeholder="Enter your massage for us here. We will get back to you within 2 business days." rows="7" required></textarea>
+                      <textarea class="form-control" id="message" name="message" placeholder="Enter your messsage for us here" value="<?php echo (isset($message) ? $message : ""); ?>" rows="7" required></textarea>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <div class="col-md-12 text-center">
-                      <button type="submit" class="submit btn btn-primary btn-lg">Submit</button>
+                      <button  name="submit" type="submit" class="submit btn btn-primary btn-lg">Submit</button>
                     </div>
                   </div>
 
