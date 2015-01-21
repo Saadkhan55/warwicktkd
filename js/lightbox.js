653043c4c02a732334e8c747110eceea9a6543f8
Haseeb Majid <hmajid2301@gmail.com>
2015-01-21 15:36:30 +0000

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
   $('.gallery-set .gallery').click(function(e) {
       e.preventDefault();
+      //Get location of picture in project directory
       var image_src = $(this).find("img").attr("src");
-      console.log(image_src);
+      
+      //add this picture to the lightbox, (inside content)
       $('#content').append('<img src="' + image_src + '" /> ');
+      //show lightbox div
       $('#lightbox').fadeIn("500");
+  });
+  
+  //click anywhere to close lightbox
+  $("#lightbox").live("click", function()  {
+    $("#lightbox").hide();
   });
 });
