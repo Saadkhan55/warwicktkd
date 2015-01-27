dfcb2c23a847bd587dd954f3e802ed52deb5029d
hmajid2301 <hmajid2301@gmail.com>
2015-01-27 23:02:55 +0000

    finished red belt

---
+++ red-belt.php
@@ -0,0 +1,548 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--Add header data (such as import stylesheets) -->
+    <?php include 'php/head.php' ?>
+    <title>Red Belt (2nd Kup)</title>
+  </head>
+  <body>
+    <div id="wrapper">
+      <!--Add header including logo, and navbar elements -->
+      <?php include 'php/header.php' ?>
+      <div class="container syllabus">
+        <div class="row">
+          <h2 class="section">Red Belt</h2>
+
+          <h3>Grading Requirements</h3>
+          <p class="larger-font">
+            <b>Techniques</b> <br><br>
+            Consecutive Kicking - Front / Back / Side / Turning (Slow & Fast Motion)<br>
+            Yonsok Chagi – Ap / Dwit / Yop/ Dollyo<br><br>
+
+            <b>Pattern</b><br><br>
+            Hwa-Rang Tul <br>
+            Examiners Choice<br><br>
+
+            <b>Sparring</b><br><br>
+            One Step sparring Ilbo Matsogi<br><br>
+
+            Flying Turning Kick & Flying Reverse Turning Kick<br>
+            Twimyo Dollyo Chagi - Twimyo Bandae Dollyo Chagia<br><br>
+
+            One Step Semi-free Sparring No 3, 4 & 5 Ilbo Jayu Matsogi<br>
+            1 x 2 Free Sparring Jayu Matsogi<br><br>
+
+            <b>Destruction</b><br><br>
+            Flying High Kick - Twimyo Nopi Chagi<br>
+            Any Hand Technique <br><br>
+
+            <b>Theory</b><br><br>
+            Korean Terminology for Your Grade<br><br>
+
+            <b>Application of Technique</b><br><br>
+            Releases from rear "Half Nelson" hold<br><br>
+          
+          </p> <!-- End of grading requirement -->
+
+          <h3>Joong-Gun<small>An Joong Gun</small></h3>
+
+          <p class="larger-font">
+            <b>  Meaning </b> <br> <br>
+            HWA-RANG is named after the Hwa-Rang youth group , which originated
+            in the Silla Dynasty in the early 7th century. The 29 movements refer to
+            the 29th Infantry Division, where TaeKwon-Do developed into maturity.<br><br>
+            <b> Number of moves: 29 </b>
+          </p>
+
+          <img src="images/pattern/2nd_kup.png" alt="" width="150px" height="150px">
+
+          <!-- Table showing off moves for the pattern -->
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Move</th>
+                <th>Stance</th>
+                <th>Section</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td></td>
+              <td><p>To Begin</p></td>
+              <td><p>Closed Ready C</p></td>
+              <td></td>
+              <td></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>1</p></td>
+              <td><p>Step To B</p></td>
+              <td><p>Sitting</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Palm Pushing Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>2</p></td>
+              <td><p></p></td>
+              <td><p>Sitting</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Right Forefist Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>3</p></td>
+              <td><p></p></td>
+              <td><p>Sitting</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Left Forefist Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p></p></td>
+              <td><p>Shift Stepping</p></td>
+              <td><p></p></td>
+              <td><p></p></td>
+              <td><p></p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>4</p></td>
+              <td><p>To A</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid/High</p></td>
+              <td><p>Twin Forearm Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>5</p></td>
+              <td><p></p></td>
+              <td><p>L</p></td>
+              <td><p>High</p></td>
+              <td><p>Upwards Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p></p></td>
+              <td><p>Sliding</p></td>
+              <td><p></p></td>
+              <td><p></p></td>
+              <td><p></p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>6</p></td>
+              <td><p>To A</p></td>
+              <td><p>Fixed</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Right Forefist Side Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>7</p></td>
+              <td><p>Slide Right Foot Up To C</p></td>
+              <td><p>Vertical</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Downward Knifehand Strike</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>8</p></td>
+              <td><p>Step To A</p></td>
+              <td><p>Walking</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Left Forefist Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>9</p></td>
+              <td><p>Step To D</p></td>
+              <td><p>Walking</p></td>
+              <td><p>Low</p></td>
+              <td><p>Outer Forearm Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>10</p></td>
+              <td><p>Step To D</p></td>
+              <td><p>Walking</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Right Forefist Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>11</p></td>
+              <td><p>Slide Left Foot Half In</p></td>
+              <td><p>Half</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Grab Right Hand, Side Piercing Kick</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>12</p></td>
+              <td><p>Land To D</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Knifehand Strike</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>13</p></td>
+              <td><p>Step To D</p></td>
+              <td><p>Walking</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Left Forefist Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>14</p></td>
+              <td><p>Step To D</p></td>
+              <td><p>Walking</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Right Forefist Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>15</p></td>
+              <td><p>Turn To E</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Knifehand Guarding Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>16</p></td>
+              <td><p>Step To E</p></td>
+              <td><p>Walking</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Fingertip Thrust</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>17</p></td>
+              <td><p>Turn To F</p></td>
+              <td><p>L</p></td>
+              <td><p>High</p></td>
+              <td><p>Knifehand Guarding Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>18</p></td>
+              <td><p></p></td>
+              <td><p></p></td>
+              <td><p>High</p></td>
+              <td><p>Turning Kick Consecutive</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>19</p></td>
+              <td><p>Land To F</p></td>
+              <td><p></p></td>
+              <td><p>High/Mid</p></td>
+              <td><p>Turning Kick Knifehand Guarding Block(Fast Motion)</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>20</p></td>
+              <td><p>Step To C</p></td>
+              <td><p>Walking</p></td>
+              <td><p>Low</p></td>
+              <td><p> Outer Forearm Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p></p></td>
+              <td><p>Sliding</p></td>
+              <td><p></p></td>
+              <td><p></p></td>
+              <td><p></p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>21</p></td>
+              <td><p>Slip To C</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Right Forefist Obverse Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>22</p></td>
+              <td><p>Step To C</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Left Forefist Obverse Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>23</p></td>
+              <td><p>Step To C</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Right Forefist Obverse Punch</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>24</p></td>
+              <td><p>Slip To C</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>X Fist Press Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>25</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Side Elbow Thrust</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>26</p></td>
+              <td><p>Slide To B</p></td>
+              <td><p>Closed</p></td>
+              <td><p>Mid/Low</p></td>
+              <td><p>Side Front Right Inner Forearm Block Left Side Front Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>27</p></td>
+              <td><p>Slide To B</p></td>
+              <td><p>Closed</p></td>
+              <td><p>Mid/Low</p></td>
+              <td><p>Side Front Left Inner Forearm Block Right Side Front Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>28</p></td>
+              <td><p>Step To B</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Knifehand Guarding Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+              <td><p>29</p></td>
+              <td><p>Step To A</p></td>
+              <td><p>L</p></td>
+              <td><p>Mid</p></td>
+              <td><p>Knifehand Guarding Block</p></td>
+            </tr> <!-- Each move -->
+
+            <tr>
+            <tr>
+              <td><p></p></td>
+              <td><p>To End</p></td>
+              <td><p>Close Ready C</p></td>
+              <td><p></p></td>
+              <td><p>Right Foot Moves</p></td>
+            </tr> <!-- Each move -->
+
+            </tbody> 
+          </table> <!-- End of Chon-Ji pattern -->
+
+
+          <div class="sparring">
+            <h3>Ilbo Matsogi <small>One Step Sparring</small></h3>
+
+            <p class="larger-font">
+              <b>Number 11</b><br><br>
+              Slide back on 45 degree angle with the right leg to form a right L-stance whilst
+              performing a forearm guarding block.<br><br>
+
+              Counter with a high flying turning kick to the head with the right leg, landing in Left Lstance
+              forearm guarding block.<br><br>
+
+              Swap for left attack.<br><br>
+
+              <b>Number 12</b><br><br>
+               Slide back on 45 degree angle with the right leg to form a right L-stance whilst
+               performing a forearm guarding block.<br><br>
+               
+               Counter with a high flying reverse turning kick to the head with the right leg, landing
+               in Right L-stance forearm guarding block.<br><br>
+
+               Swap for a left attack.<br><br>
+            </p>
+
+            <h3>Ilbo Jayu Matsogi <small>One Step Semi-Free Sparring</small></h3>
+            <p class="larger-font">
+
+              <b> Number 3</b> <br><br>
+              <b>Attacker:</b> steps forward low front snap kick with the right leg.<br><br>
+
+              <b>Defender:</b> step back into left walking stance performing an x-fist pressing block followed by
+              a middle reverse punch with the right hand.<br><br>
+
+              <b>Attacker:</b> before you land the kicking leg, block with a reverse outer forearm block quickly
+              followed by a high obverse punch with the right hand as the right foot hits the ground. Kihap
+              on the punch.<br><br>
+
+              <b> Number 4 </b> <br><br>
+              <b>Attacker:</b> steps forward middle side piercing kick from the back (right) leg. Land in sitting
+              stance and perform a back elbow thrust with the left elbow.<br><br>
+
+              <b>Defender:</b> assume a left L-stance while dodging to the outside of the kick, performing an
+              outwards knifehand strike to the back on the neck.<br><br>
+
+              <b>Defender:</b> step forward with your right foot then left foot (double stepping), pivot on the
+              right and perform a high turning kick with the left leg. Kihap on the kick<br><br>
+
+              <b> Number 5</b><br><br>
+              <b>Attacker:</b> steps forward into a right fixed stance middle forefist side punch.<br><br>
+
+              <b>Defender:</b> Assume a left rear foot stance forearm guarding block. Pivot on the right foot
+              executing a high reverse turning kick with the left leg.<br><br>
+
+              <b>Attacker:</b> execute a high grasping block. Step your left leg behind your right into an xstance
+              while still holding the leg. This should push your hips through the opponent causing
+              him or her to fall to the floor. Direct the leg aside and perform a low side piercing kick with
+              the right leg to the groin. <br><br>
+            </p>
+        </div> <!-- End of sparring -->
+      
+          <div class="theory">
+            <h3>Theory</h3>
+            <p class="larger-font">
+              Note: You may be asked questions on lower grade theory, so make sure you know it.<br> <br>
+
+              To Hide or show korean click this button to help you study
+              <button class="btn btn-primary">Hide</button>
+              </p><br><br>
+
+            <!-- Korean theory with english translation -->
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>Technique</th>
+                  <th>Korean </th>
+                </tr>
+              </thead>
+
+              <tbody>
+
+                <tr class="title">
+                  <td>Stance</td>
+                  <td>Sogi</td>
+                </tr>
+
+                <tr>
+                  <td>Vertical Stance</td>
+                  <td>Soojik Sogi</td>
+                </tr>
+
+                <tr class="title">
+                  <td>Kicks</td>
+                  <td>Chagi</td>
+                </tr>
+
+                <tr>
+                  <td>Flying Reverse Side Kick</td>
+                  <td>Twimyo Bandae Yop Chagi</td>
+                </tr>
+
+                <tr>
+                  <td>Flying Reverse Turning Kick</td>
+                  <td>Twimyo Bandae Dollyo Chagi</td>
+                </tr>
+
+                <tr class="title">
+                  <td>Strike</td>
+                  <td>Taerigi</td>
+                </tr>
+
+                <tr>
+                  <td>Downward Knifehand Strike</td>
+                  <td>Naeryo Sonkal Taerigi</td>
+                </tr>
+
+                <tr class="title">
+                  <td>Block</td>
+                  <td>Makgi</td>
+                </tr>
+
+                <tr>
+                  <td>Grasping Block Block</td>
+                  <td>Butjaba Makgi</td>
+                </tr>
+
+                <tr>
+                  <td>Side Front Block</td>
+                  <td>Yopap Makgi</td>
+                </tr>
+
+                <tr>
+                  <td>Palm Pushing Block</td>
+                  <td>Sonbadak Miro Makgi</td>
+                </tr>
+
+                <tr class="title">
+                  <td>Punch</td>
+                  <td>Jirugi</td>
+                </tr>
+
+                <tr>
+                  <td>Upward Punch</td>
+                  <td>Ollyo Jirugi</td>
+                </tr>
+
+                <tr>
+                  <td>Twin Upset Punch</td>
+                  <td>Sang Dwijibo Jirugi</td>
+                </tr>
+
+                <tr class="title">
+                  <td>Thrust</td>
+                  <td>Tulgi</td>
+                </tr>
+
+                <tr>
+                  <td>Twin Side Elbow Thrust</td>
+                  <td>Sang Yop Palkup Tulgi</td>
+                </tr>
+
+                <tr class="title">
+                  <td>Other</td>
+                  <td>Other</td>
+                </tr>
+
+                <tr>
+                  <td>Sliding</td>
+                  <td>Mikulgi</td>
+                </tr>
+
+                <tr>
+                  <td>Consecutive Motion</td>
+                  <td>Yonsok Dongjak</td>
+                </tr>
+
+                <tr>
+                  <td>Shift Stepping</td>
+                  <td>Jajunbal Didigi</td>
+                </tr>
+
+                <tr>
+                  <td>Step Stepping</td>
+                  <td>Omgyo Didimyo Dolgi</td>
+                </tr>
+
+                <tr>
+                  <td>Double Stepping</td>
+                  <td>Ibo Omgyo Didigi</td>
+                </tr>
+              </tbody>
+            </table> <!-- End of table -->
+          </div> <!-- End of theory -->
+
+        </div> <!-- End of row -->
+      </div> <!-- End of container -->
+    </div><!--End of wrapper -->
+    <!--Add footer, contains 3 columns layout Add footer data, such as jquery, boostrap javascript -->
+    <?php include 'php/footer.php' ?>
+    <script src="js/theory.js"></script>
+  </body>
+</html>
+
