4da561d83d203c5b6da7f2c531ce958ae910b2d0
Saadkhan55 <Saadkhan051096@gmail.com>
2017-08-17 20:33:39 +0100

    Add files via upload
    Minor changes, fixed incorrect links and added new times and locations for training

--- training.php
+++ training.php
@@ -16,14 +16,15 @@
 
             <h3>When and Where?</h3><br>
             <p class="larger-font">  
-              Tuesday: 20:30 - 22:00 <br>
-              Thursday: 19:00 - 20:30 <br>
-              Saturday: 14:00 - 16:00 <br>
+              Monday (WTF) : 20:30 - 22:00 <br>
+              Tuesday(ITF) : 18:00 - 19:30 <br>
+              Thursday (ITF) : 19:30 - 21:00 <br>
             </p>
 
             <p class="larger-font">
-            All sessions are at <a href="images/map.jpg">Westwood Games Hall</a>. Try to turn up to lessons 10-15 minutes early. Please wear a clean dobok (uniform) to each formal training session. Beginners, please bring some loose-fitting clothing such as a t-shirt and shorts/trousers to provide freedom of movement. As a safety requirement, training is done barefoot and we request that you remove any jewellery. <br>
-            </p>
+            <!-- All sessions are at <a href="images/map.jpg">Westwood Games Hall</a>. Try to turn up to lessons 10-15 minutes early. Please wear a clean dobok (uniform) to each formal training session. Beginners, please bring some loose-fitting clothing such as a t-shirt and shorts/trousers to provide freedom of movement. As a safety requirement, training is done barefoot and we request that you remove any jewellery. <br> -->
+            Monday and Thursday sessions are in the Activity Room, Tueday in the Main sports hall boh in the <a href="images/map2.jpg">Sports Centre</a>. Please wear a clean dobok (uniform) to each ITF training session, loose fitting sports kit is fine for WTF. Beginners, please bring some loose-fitting clothing such as a t-shirt and shorts/trousers to provide freedom of movement. As a safety requirement, training is done barefoot and we request that you remove any jewellery. <br>
+            </p>  
 
             <h3>Cost</h3><br>
             <p class="larger-font">
@@ -32,7 +33,7 @@
             Per Term: £30 <br>
             Per Year: £60 <br> <br>
 
-            To officially join the club (and be covered by the Warwick Sport insurance), login to our   <a href="http://warwicksport.warwick.ac.uk/clubs/taekwondo/" target="_blank">Warwick Sports page </a>and follow the instructions. <br><br>
+            To officially join the club (and be covered by the Warwick Sport insurance), login to our   <a href="https://www.warwicksu.com/sports/clubs/taekwondo/" target="_blank">Warwick Sports page </a>and follow the instructions. <br><br>
 
             If you wish to grade to a higher belt you will eventually need to buy a dobok (the white suit) which can be purchased through the club. If you want to pursue sparring then protective gear is available for purchase in Term 2 ,although this is not essential to continue training.
             </p>
