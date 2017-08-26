c1cf5461bcc10193003f6b83d4a92a8b31012d0f
Saadkhan55 <Saadkhan051096@gmail.com>
2017-08-26 17:52:46 +0100

    Changed way bootstrap added
    original was not working, amended

--- php/head.php
+++ php/head.php
@@ -2,7 +2,9 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="images/favicon.ico" rel="shortcut icon">
-<link href="css/vendor/bootstrap.min.css" rel="stylesheet" media="all">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link href="css/vendor/font-awesome.min.css" rel="stylesheet">
 <link href="css/vendor/animate.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet" media="all">
